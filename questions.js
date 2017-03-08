957f07ecf472a4be86903976236a1d2c587f7a05
Jerry Housh <housga12@wfu.edu>
2017-03-08 08:33:20 -0600

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,113 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+
+    "QUESTIONS_EN_US" : [
+        {
+            "What is Gazorpazorpfield's favorite food?": [
+                "enchiladas",
+                "tacos",
+                "meatloaf",
+                "lasagna",
+                "pizza",
+                "steak"
+            ]
+        },
+
+        {
+            "What was Ricks favorite exhibit in Anatomy Park?": [
+                "pirates of the pancreas",
+                "bladder falls",
+                "the bone train",
+                "spleen mountain"
+            ] 
+        },
+
+        {
+            "What does snuffles want to be called now that he abandoned his slave name?": [
+                "snowball",
+                "snowbot",
+                "whitefur",
+                "morty the dog"
+            ]
+        },
+
+        {
+            "Who is the voice actor for both Rick Sanchez and Morty Smith?": [
+                "Justin Roiland",
+                "Chris Parnell",
+                "Spencer Grammer",
+                "Sarah Chalke"
+            ]
+        },
+
+        {
+            "What color is Ricks hair?": [
+                "blue",
+                "brown",
+                "white",
+                "orange"
+                ]
+        },
+
+        {
+            "Who does Morty have a crush on?": [
+                "Jessica",
+                "Sarah",
+                "Amy",
+                "Your mom"
+            ]
+        },
+
+        {
+            "What dimension are Rick and Morty from?": [
+                "C one thirty seven",
+                "C sixty eight",
+                "C three fourty two",
+                "C five seventy nine"
+            ]
+        },
+
+        {
+            "What is Misses Pancakes mantra?": [
+                "You dont knnnnnoww me",
+                "I just like killin",
+                "Look at me",
+                "ewwwwwwee",
+                "wubba lubba dub dub"
+            ]
+        },
+        
+        {
+            "How much does a ride from Slippery Stairs cost?": [
+                "25 Schmeckles",
+                "50 Schmeckles",
+                "20 Schmeckles",
+                "75 Schmeckles"
+            ]
+        },
+        {
+            "Who was not a parasite in episode fifteen, total rickall?": [
+                "Mister Poopybutthole",
+                "Photography Raptor",
+                "Pencilvestyr",
+                "Mister Beauregard",
+                "Sleepy Gary",
+                "Cousin Nicky"
+            ]
+        },
+        {
+            "What is the Smith family\'s cereal of choice?": [
+                "Strawberry Smiggles",
+                "Eyeholes",
+                "Cheerios",
+                "Frosted Flakes"
+            ]
+        },
+       
+    ],
+};
